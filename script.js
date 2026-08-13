e51ea9fb715a2e27806489fe1afd13874fb51fda
YoungHobbyist <aryanhashimi06@gmail.com>
2026-08-12 23:20:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ const REVIEW_URL =
 const SURVEY_URL =
   "https://take.supersurvey.com/Q8NGAF0TL";
 
-
 const reviewButton =
   document.getElementById("reviewButton");
 
 const surveyButton =
   document.getElementById("surveyButton");
-
 
 reviewButton.href = REVIEW_URL;
 
